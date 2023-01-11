49d4f1f5fa763a51ba47ec522cd622f688bbe96d
abe <abe@futurewonder.com>
2023-01-10 23:34:06 -0500

    introduce a simple go gin backend api for demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const backendUrl = 'http://127.0.0.1:8080/hello';
+  fetch(backendUrl).then(res => console.log(res));
+  
   return (
     <div className="App">
       <header className="App-header">
