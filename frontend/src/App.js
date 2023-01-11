a255e686199c09b4dc7aa922964d19f11b829c74
abe <abe@futurewonder.com>
2023-01-10 23:03:48 -0500

    serve react through nginx
    default port 3000

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Boogey was here. Change App.js and save to reload.
+          Boogey, duba. Change App.js and save to reload.
         </p>
       </header>
     </div>
