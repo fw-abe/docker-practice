3ab24b08540c68d030e2d48dbcc28ab837cf612e
abe <abe@futurewonder.com>
2023-01-16 20:21:56 -0500

    docker powered ssl attept

---
+++ renew-cert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. ../.env;
+if [[ ! -d ../nginx/certbot-webroot ]]; then
+    mkdir ../nginx/certbot-webroot;
+fi;
+docker compose -f ../docker-compose.yml -f ../docker-compose.prod.yml run \
+    --entrypoint certbot \
+    certbot renew -a webroot --webroot-path /var/www;
+
+# Sending a SIGHUP signal to the container using docker container kill --signal=HUP command, 
+# will cause the running nginx process inside the container to reload its configuration without 
+# needing to restart the container. This can be useful because it allows you to reload the configuration 
+# without losing any established connections or in-memory data.
+docker container kill --signal=HUP dm-nginx;
--- start-prod.sh
+++ start-prod.sh
@@ -11,4 +11,4 @@ fi
 
 FILE_ARGS=(-f docker-compose.yml -f docker-compose.prod.yml);
 DOCKER_BUILDKIT=1 docker compose "${FILE_ARGS[@]}" build --parallel || exit;
-docker compose "${FILE_ARGS[@]}" --env-file "${ENV_FILE}" up;
+docker compose "${FILE_ARGS[@]}" --env-file "${ENV_FILE}" up -d;
